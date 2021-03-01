4cff4855c4d3677b69d39921d760ba85d7c7dda0
Burak <isikburak142@gmail.com>
2021-03-01 10:44:23 +0100

    Resolution of exercise 5 after debugging

--- 1-JavaScript/Week4/homework/ex5-wallet/index.js
+++ 1-JavaScript/Week4/homework/ex5-wallet/index.js
@@ -25,7 +25,7 @@ function createWallet(name, cash = 0) {
   function transferInto(wallet, amount) {
     console.log(
       `Transferring ${eurosFormatter.format(amount)} from ${name} to ${
-        wallet.name
+        wallet.getName()
       }`
     );
     const withdrawnAmount = withdraw(amount);
@@ -77,7 +77,7 @@ const quiz = {
       b: 'cash, name', 
       c: 'amount, this, wallet'
     },
-    answer: c,
+    answer: b,
   },
   q2: {
     question: 'What is in the Call Stack, from top to bottom?',
@@ -104,7 +104,7 @@ const quiz = {
       b: 'Transferring € 50,00 from Jack to undefined', 
       c: 'Transferring € 50,00 from Jack to Jane' 
     },
-    answer: c,
+    answer: a,
   },
   q5: {
     question: 'The owner of the wallet with insufficient funds is:',
@@ -113,6 +113,6 @@ const quiz = {
       b: 'Joe', 
       c: 'Jane' 
     },
-    answer: b,
+    answer: c,
   },
 };
